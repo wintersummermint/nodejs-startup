471f2fb91561cc3d4908456f9c1aaec71764cfb8
Nixon Gwin Galario <nixongalario@gmail.com>
2015-12-26 10:00:21 +0000

    eventEmitter

---
+++ eventEmitter/index.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var EventEmitter = require("event").EventEmitter;
+
+http.createServer(function(req,res){
+    res.writeHead(200);
+    console.log('dwqqd');
+}).listen(8080);
